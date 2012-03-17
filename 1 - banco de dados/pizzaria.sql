bb09c20672eab55865b00efcbe975dedb28eedfd
Julio Makdisse Saito <saitodisse@gmail.com>
2012-03-17 12:06:24 -0300

    drop if exist

--- 1 - banco de dados/pizzaria.sql
+++ 1 - banco de dados/pizzaria.sql
@@ -1,18 +1,20 @@
-#DROP DATABASE pizzaDB;  #somente execute isto para recriar o banco
+DROP DATABASE IF EXISTS pizzaDB;
 CREATE DATABASE pizzaDB;
 
-CREATE TABLE IF NOT EXISTS `pizzaDB`.`Pizza` (
+USE pizzaDB;
+
+CREATE TABLE IF NOT EXISTS `Pizza` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `Nome` VARCHAR(45) NULL ,
   `Preco` DECIMAL(8,2) NULL ,
   PRIMARY KEY (`id`) );
 
-CREATE  TABLE IF NOT EXISTS `pizzaDB`.`Ingrediente` (
+CREATE  TABLE IF NOT EXISTS `Ingrediente` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `Nome` VARCHAR(45) NULL ,
   PRIMARY KEY (`id`) );
 
-CREATE  TABLE IF NOT EXISTS `pizzaDB`.`Pizza_Ingrediente` (
+CREATE  TABLE IF NOT EXISTS `Pizza_Ingrediente` (
   `Pizza_id` INT NOT NULL ,
   `Ingrediente_id` INT NOT NULL ,
   PRIMARY KEY (`Pizza_id`, `Ingrediente_id`) ,
@@ -20,38 +22,38 @@ CREATE  TABLE IF NOT EXISTS `pizzaDB`.`Pizza_Ingrediente` (
   INDEX `fk_Pizza_Ingrediente_Pizza` (`Pizza_id` ASC) ,
   CONSTRAINT `fk_Pizza_Ingrediente_Pizza`
     FOREIGN KEY (`Pizza_id` )
-    REFERENCES `pizzaDB`.`Pizza` (`id` )
+    REFERENCES `Pizza` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Pizza_Ingrediente_Ingrediente1`
     FOREIGN KEY (`Ingrediente_id` )
-    REFERENCES `pizzaDB`.`Ingrediente` (`id` )
+    REFERENCES `Ingrediente` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
-INSERT INTO `pizzaDB`.`Pizza` (`id`, `Nome`, `Preco`) VALUES (1, 'Muçarela', 10);
-INSERT INTO `pizzaDB`.`Pizza` (`id`, `Nome`, `Preco`) VALUES (2, 'Portuguesa', 20);
-INSERT INTO `pizzaDB`.`Pizza` (`id`, `Nome`, `Preco`) VALUES (3, 'Calabreza', 12);
+INSERT INTO `Pizza` (`id`, `Nome`, `Preco`) VALUES (1, 'Muçarela', 10);
+INSERT INTO `Pizza` (`id`, `Nome`, `Preco`) VALUES (2, 'Portuguesa', 20);
+INSERT INTO `Pizza` (`id`, `Nome`, `Preco`) VALUES (3, 'Calabreza', 12);
 
 
-INSERT INTO `pizzaDB`.`Ingrediente` (`id`, `Nome`) VALUES (1, 'queijo');
-INSERT INTO `pizzaDB`.`Ingrediente` (`id`, `Nome`) VALUES (2, 'calabreza');
-INSERT INTO `pizzaDB`.`Ingrediente` (`id`, `Nome`) VALUES (3, 'cebola');
-INSERT INTO `pizzaDB`.`Ingrediente` (`id`, `Nome`) VALUES (4, 'ovo');
-INSERT INTO `pizzaDB`.`Ingrediente` (`id`, `Nome`) VALUES (5, 'azeitona');
-INSERT INTO `pizzaDB`.`Ingrediente` (`id`, `Nome`) VALUES (6, 'molho de tomate');
+INSERT INTO `Ingrediente` (`id`, `Nome`) VALUES (1, 'queijo');
+INSERT INTO `Ingrediente` (`id`, `Nome`) VALUES (2, 'calabreza');
+INSERT INTO `Ingrediente` (`id`, `Nome`) VALUES (3, 'cebola');
+INSERT INTO `Ingrediente` (`id`, `Nome`) VALUES (4, 'ovo');
+INSERT INTO `Ingrediente` (`id`, `Nome`) VALUES (5, 'azeitona');
+INSERT INTO `Ingrediente` (`id`, `Nome`) VALUES (6, 'molho de tomate');
 
 # muçarela
-INSERT INTO `pizzaDB`.`Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (1, 6);
-INSERT INTO `pizzaDB`.`Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (1, 1);
+INSERT INTO `Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (1, 6);
+INSERT INTO `Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (1, 1);
 
 # portuguesa
-INSERT INTO `pizzaDB`.`Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (2, 6);
-INSERT INTO `pizzaDB`.`Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (2, 3);
-INSERT INTO `pizzaDB`.`Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (2, 4);
-INSERT INTO `pizzaDB`.`Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (2, 5);
+INSERT INTO `Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (2, 6);
+INSERT INTO `Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (2, 3);
+INSERT INTO `Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (2, 4);
+INSERT INTO `Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (2, 5);
 
 # calabreza
-INSERT INTO `pizzaDB`.`Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (3, 6);
-INSERT INTO `pizzaDB`.`Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (3, 2);
+INSERT INTO `Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (3, 6);
+INSERT INTO `Pizza_Ingrediente` (`Pizza_id`, `Ingrediente_id`) VALUES (3, 2);
