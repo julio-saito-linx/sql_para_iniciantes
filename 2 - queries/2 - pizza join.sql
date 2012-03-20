4276722b2f8da36ec14ed0de2153e1593b96be2e
saitodisse <saitodisse@gmail.com>
2012-03-20 14:18:28 -0300

    join mais bonito

--- 2 - queries/2 - pizza join.sql
+++ 2 - queries/2 - pizza join.sql
@@ -1,5 +1,11 @@
 -- pizza ligada aos ingredientes pela pizza_ingrediente
-select * from pizza
-join pizza_ingrediente on Pizza_id = pizza.id
-join ingrediente on pizza_ingrediente.ingrediente_id = ingrediente.id
-where pizza.Nome = 'Muçarela';
+select 
+    pizza.Nome as 'Nome da Pizza',
+    ingrediente.Nome as 'Ingredientes'
+from
+    pizza
+        join
+    pizza_ingrediente ON Pizza_id = pizza.id
+        join
+    ingrediente ON pizza_ingrediente.ingrediente_id = ingrediente.id
+order by pizza.Nome;
